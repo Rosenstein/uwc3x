18e4402d653bb19987fce82af065c2121e312416
Rosenstein <Rosenstein@users.noreply.github.com>
2024-01-14 14:23:18 +0100

    set error reporting

--- web_files/uwc3stats/index.php
+++ web_files/uwc3stats/index.php
@@ -1,5 +1,9 @@
 <?php
 
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
+	ini_set('log_errors', 'On');
+
 	include_once("include/header.php");
 
 	$start = isset($_GET["s"]) ;		// first player on the page
