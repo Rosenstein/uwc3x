c413c6d83a03b345ea69ac5f691a8cfef7dc62b9
Rosenstein <Rosenstein@users.noreply.github.com>
2024-01-14 14:23:38 +0100

    fix for undefined variable

--- web_files/uwc3stats/xptable.php
+++ web_files/uwc3stats/xptable.php
@@ -3,7 +3,7 @@
 <head>
 <title>XP Table</title>
 <meta charset="UTF-8">
-<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
@@ -12,6 +12,7 @@
 </center>
 <br />
 	<?php
+		require_once("./include/config.php");
 		require_once("./include/functions.php");
 		ShowXPTable();
 	?>
